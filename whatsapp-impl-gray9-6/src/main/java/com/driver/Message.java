565f032d5fe8d2be413d1798d349c863fda7d478
Ajay kumar <123147364+gray9-6@users.noreply.github.com>
2023-06-29 09:15:22 +0530

    Add files via upload

---
+++ whatsapp-impl-gray9-6/src/main/java/com/driver/Message.java
@@ -0,0 +1,27 @@
+package com.driver;
+
+import java.util.Date;
+
+public class Message {
+    private int id;
+    private String content;
+    private Date timestamp;
+
+    public Message(int id, String content) {
+        this.id = id;
+        this.content = content;
+        this.timestamp = new Date();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public Date getTimestamp() {
+        return timestamp;
+    }
+}
